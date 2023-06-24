55a4b7c3dfd7fde93da6c2cf0539598b9f0e6c69
AnnaKolmychek <a.kolmychek@gmail.com>
2023-06-24 21:11:53 +0300

    create project structure

---
+++ parser_vacancies/classes/job_sites_api.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+
+class JobSitesAPI(ABC):
+    pass
+# TODO подключение к API
+
+# TODO получение вакансий
+
+
+class HeadHunterAPI(JobSitesAPI):
+    pass
+
+
+class SuperJobAPI(JobSitesAPI):
+    pass
+
---
+++ parser_vacancies/main.py
@@ -0,0 +1,6 @@
+import parser_vacancies.utils.user_interaction
+
+if __name__ == '__main__':
+    pass
+    # TODO запуск взаимодействия с пользователем
+
