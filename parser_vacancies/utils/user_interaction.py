be45bdbffae63f7beee2d40b5518303101191542
AnnaKolmychek <a.kolmychek@gmail.com>
2023-06-25 21:31:54 +0300

    add ыÐstructure params for user search

--- parser_vacancies/utils/user_interaction.py
+++ parser_vacancies/utils/user_interaction.py
@@ -5,3 +5,11 @@
 #  получить топ N вакансий по зарплате,
 #  получить вакансии в отсортированном виде,
 #  получить вакансии, в описании которых есть определенные ключевые слова, например "postgres" и т. п.
+
+# user_search_params = {'town': int,
+#                       'keywords': str | list,
+#                       'payment': int,
+#                       'only_with_payment': bool,
+#                       'distant work': bool,
+#                       'day_from': int,
+#                       }
